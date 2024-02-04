913b4099d1b5d81e7240f70a5add385ad4ead934
amelia2802 <amelia280111dutta@gmail.com>
2024-02-04 21:07:09 +0530

    solve mod 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    if(inputStr.length==0){
+        return ''
+    }
+    
+    return inputStr.split('').reverse().join('')
 };
 
 // Do not edit below this line
